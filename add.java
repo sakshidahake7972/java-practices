7fc438a63d0a2726205f6e7d020b3cdd2c3f5854
sakshidahake7972 <dahakesakshi43@gmail.com>
2026-02-05 16:37:18 +0530

    this is a basic code programm

--- add.java
+++ add.java
@@ -6,3 +6,26 @@ public class Add {
         System.out.println(sum);
     }
 }
+
+
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n = 4;
+
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
+
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
