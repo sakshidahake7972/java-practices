31d25ed2e015b0d0ad0fcca57c3313a86608dcb4
sakshidahake7972 <dahakesakshi43@gmail.com>
2026-03-25 21:55:15 +0530

    Add files via upload

---
+++ 1 class Person.java
@@ -0,0 +1,22 @@
+class Person {
+    String name;
+    int age;
+
+    // Constructor
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Sakshi", 20);
+        Person p2 = new Person("Rahul", 22);
+
+        p1.display();
+        p2.display();
+    }
+}
