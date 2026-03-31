e77f81e257bb24b16abf9c8f19a35c26c404ec8d
sakshidahake7972 <dahakesakshi43@gmail.com>
2026-03-31 22:13:05 +0530

    Add files via upload

---
+++ StudentInputSystem.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class StudentInputSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter age: ");
+            int age = sc.nextInt();
+            System.out.println("Student age is: " + age);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic error");
+        }
+    }
+}
